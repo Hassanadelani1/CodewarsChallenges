43bbd18683dbe069e598d57d24df0effe23dff91
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-03-10 16:08:48 +0200

    Update solution - CodeHub

--- 6-kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.js
+++ 6-kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.js
@@ -1,4 +1,6 @@
 function digitalRoot(n) {
+  if(n < 10) return n
   
-  return n === 0? 0 : 1 + (n - 1) % 9;
+  return digitalRoot(n.toString().split("").map(Number).reduce((sum, current)=> sum + current, 0))
+//   return n === 0? 0 : 1 + (n - 1) % 9;
 }

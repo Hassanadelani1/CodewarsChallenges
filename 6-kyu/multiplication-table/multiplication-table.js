03fa892e65648bd66abed56a05d20f9cff77d9dc
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-03-26 13:14:42 +0200

    Add solution - CodeHub

---
+++ 6-kyu/multiplication-table/multiplication-table.js
@@ -0,0 +1,12 @@
+function multiplicationTable(size) {
+  const table = []
+  for(let i = 1; i <= size; i++){
+    let row = []
+    for(let j = 1; j <= size; j++){
+      row.push(i * j)
+    }
+    table.push(row)
+  }
+  return table
+}
+​

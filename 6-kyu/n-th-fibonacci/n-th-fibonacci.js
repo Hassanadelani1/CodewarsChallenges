8ad6508442df519c2462c6174af8eaaa6062b353
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-08 15:41:38 +0200

    Update solution - CodeHub

--- 6-kyu/n-th-fibonacci/n-th-fibonacci.js
+++ 6-kyu/n-th-fibonacci/n-th-fibonacci.js
@@ -1,6 +1,8 @@
  
 function nthFibo(n) {
   // Return the n-th number in the Fibonacci Sequence
+  if(n == 1) return 0
+  if(n == 2) return 1
   let signature = [0, 1]
   for(let i = 2; i <= n; i++){
     signature[i] = signature[i] + signature[i -1]

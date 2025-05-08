34b835b46b70d96e6d901158de6adeecec73475a
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-08 15:53:11 +0200

    Update solution - CodeHub

--- 6-kyu/n-th-fibonacci/n-th-fibonacci.js
+++ 6-kyu/n-th-fibonacci/n-th-fibonacci.js
@@ -1,11 +1,14 @@
  
 function nthFibo(n) {
   // Return the n-th number in the Fibonacci Sequence
-  if(n == 1) return 0
+  let fib = [0, 1]
+  if(n == 1) return 1
   if(n == 2) return 1
-  let signature = [0, 1]
-  for(let i = 1; i <= n; i++){
-    signature[i] = signature[i] + signature[i -1]
+  for(let i = 2; i <= n; i++){
+    fib[i] = fib[i - 1] + fib[i - 2]
   }
-  return signature[signature.length - 1]
+  return fib
 }
+//loop through the fib starting from the 2 n number of times
+//for each iteration, populate the fib with the current fib value 
+//then return the last fib element

0533730c02a1d4112f868f5da2c2e262ef3de672
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-13 15:06:04 +0200

    Add solution - CodeHub

---
+++ 5-kyu/maximum-subarray-sum/maximum-subarray-sum.js
@@ -0,0 +1,22 @@
+var maxSequence = function(arr){
+    const map = new Map()
+    for(let i = 0; i < arr.length - 1; i++){
+        let sum = arr[i] + arr[i + 1]
+        map.set([arr[i], arr[i + 1]], sum)
+    }
+    let sums = []
+    let entries =  map.entries()
+    for(let arr of entries){
+        sums.push(arr[1])
+    }
+    let highestSum = Math.max(...sums)
+    
+    entries =  map.entries()
+    let result = []
+    for(let arr of entries){
+        if(arr[1] === highestSum){
+            result.push(arr[0])
+        }
+    }
+    return result.flat()
+}

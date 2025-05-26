8f7116dc191347612d78f2ad3737f32127690910
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-26 14:48:22 +0200

    Update solution - CodeHub

--- 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
+++ 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -2,9 +2,10 @@
 function replace(s) {
   //coding and coding....
   let vowels = ["a", "e", "i", "o", "u"]
-  return s.split("").map((letter)=> {
-    if(vowels.includes(letter.toLowerCase())){
-      letter = "!"
-    }
-  })
+//   return s.split("").map((letter)=> {
+//     if(vowels.includes(letter.toLowerCase())){
+//       letter = "!"
+//     }
+//   })
+  return s
 }

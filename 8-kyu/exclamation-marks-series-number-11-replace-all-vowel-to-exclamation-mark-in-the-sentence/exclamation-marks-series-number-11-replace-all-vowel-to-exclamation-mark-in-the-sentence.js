b5984d6060ae3c58aa11f9045b399f259ec3395d
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-26 14:44:00 +0200

    Add solution - CodeHub

---
+++ 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -0,0 +1,9 @@
+function replace(s) {
+  //coding and coding....
+  let vowels = /aeiou/gi
+  return s.split("").map((letter)=> {
+    if(vowels.test(letter)){
+      s.replace(letter, "!")
+    }
+  })
+}

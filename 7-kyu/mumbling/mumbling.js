6289fe0b3ea89060a61ce718390b8d0b8c111ab1
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-28 14:09:09 +0200

    Update solution - CodeHub

--- 7-kyu/mumbling/mumbling.js
+++ 7-kyu/mumbling/mumbling.js
@@ -1,7 +1,7 @@
 function accum(s) {
-  let result = ""
-  for(let i = 0; i < s.length; i++){
-    result += s[i].toUpperCase() + s[i].repeat(i)
+  let result = []
+  for(let i = 1; i <= s.length; i++){
+    result.push(s[i].toUpperCase() + s[i].repeat(i))
   }
-  return result
+  return result.join("-")
 }

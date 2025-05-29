9250ae706b1a73dcdd35c0720bfb6b19b0502156
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-05-29 13:50:48 +0200

    Update solution - CodeHub

--- 6-kyu/colorful-number/colorful-number.js
+++ 6-kyu/colorful-number/colorful-number.js
@@ -1,13 +1,16 @@
 function colourful(number) {
-  number = String(number)
-  const arr = [...number]
-  for(let i = 0; i < number.length - 1; i++){
-    let multiplication = number[i] * number[i + 1]
-    arr.push(multiplication)
+  const digits = [...String(number)].map(Number);
+  const products = new Set();
+​
+  for (let i = 0; i < digits.length; i++) {
+    let product = 1;
+    for (let j = i; j < digits.length; j++) {
+      product *= digits[j];
+      if (products.has(product)) return false;
+      products.add(product);
+    }
   }
-  let allProduct = String(number).split("").reduce((acc, current)=> acc * current,1)
-    arr.push(allProduct)
-    let products = arr.map(elem=> Number(elem))
-    let uniqueProducts = new Set(products)
-    return uniqueProducts.size === products.length
+​
+  return true;
 }
+​

db22b3c43b4b6cdb5c951f533354c7d34a1617c9
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-06-04 15:52:06 +0200

    Add solution - CodeHub

---
+++ 6-kyu/jokes-youve-been-awaiting-for-dot-dot-dot-promise/jokes-youve-been-awaiting-for-dot-dot-dot-promise.js
@@ -0,0 +1,22 @@
+ 
+async function sayJoke(apiUrl, jokeId){
+  // use mocked `fetch(url)`
+  try{
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    if(!data){
+      throw new Error(`No Jokes at url: ${apiUrl}`)
+    }
+    
+    const joke = data.find(j=> j.id === jokeId)
+    if(!joke) throw new Error(`No jokes found id: ${jokeId}`)
+    
+    return {
+      saySetup: ()=> joke.setup,
+      sayPuncLine: ()=> joke.sayPunchLine
+    }
+  }catch(error){
+    throw error
+  }
+  
+}

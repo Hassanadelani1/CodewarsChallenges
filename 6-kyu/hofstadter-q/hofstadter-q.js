0f956ed2679806880de869c8744b1848ce3f4cf8
Hassan Adelani Luqman <luqmanadelani1@gmail.com>
2025-07-30 17:49:32 +0200

    Update solution - CodeHub

--- 6-kyu/hofstadter-q/hofstadter-q.js
+++ 6-kyu/hofstadter-q/hofstadter-q.js
@@ -1,4 +1,5 @@
+ 
 function hofstadterQ(n) {
   if(n === 1 || n === 2) return 1
-  return hofstadterQ(n - Hofstadter sequences(n - 1)) +  hofstadterQ(n - Hofstadter sequences(n - 2)) 
+  return hofstadterQ(n - hofstadterQ(n - 1)) +  hofstadterQ(n - hofstadterQ(n - 2))
 }
